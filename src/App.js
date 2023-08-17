5eb9928d5300f0cb9b9b5ebbaa46a4006c53a0a5
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-08-17 16:58:56 +0200

    Refactor Form for input state and calculation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Form from "./components/Form/Form";
 import React from "react";
 import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
 import Table from "./components/Table/Table";
 
 function App() {
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
+const initialUserInput = {
+  "current-savings": 0,
+  "yearly-contribution": 0,
+  "expected-return": 0,
+  duration: 0,
+};
+
 const Form = () => {
-  const calculateHandler = (userInput) => {
+  const [userInput, setUserInput] = useState(initialUserInput);
+
+  const resetHandler = () => {
+    setUserInput(initialUserInput);
+  };
+
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
+    console.log(userInput);
+  };
+
+  const calculateHandler = (e) => {
+    e.preventDefault();
+    console.log(userInput);
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
 
-    const yearlyData = []; // per-year results
+    const yearlyData = [];
 
     let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
     const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
@@ -27,18 +52,33 @@ const Form = () => {
     }
 
     // do something with yearlyData ...
+    console.log(yearlyData);
   };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={calculateHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={userInput["current-savings"]}
+            onChange={(e) =>
+              inputChangeHandler("current-savings", e.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={userInput["yearly-contribution"]}
+            onChange={(e) =>
+              inputChangeHandler("yearly-contribution", e.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
@@ -46,15 +86,27 @@ const Form = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={userInput["expected-return"]}
+            onChange={(e) =>
+              inputChangeHandler("expected-return", e.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={userInput["duration"]}
+            onChange={(e) => inputChangeHandler("duration", e.target.value)}
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetHandler}>
           Reset
         </button>
         <button type="submit" className="button">
